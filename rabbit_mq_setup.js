613b7097485b062d72f523f57c6c184f0d6aedec
marlon101tkm <50722508+marlon101tkm@users.noreply.github.com>
2020-12-16 19:33:36 -0300

    mudanças no setup do rabbitmq e mudancas no web services

--- rabbit_mq_setup.js
+++ rabbit_mq_setup.js
@@ -14,15 +14,22 @@ async function setup() {
   let channel = await connection.createChannel();
 
   // create exchange
-  await channel.assertExchange("processing", "direct", { durable: true });
+  //await channel.assertExchange("processing", "direct", { durable: true });
+  await channel.assertExchange("topic_logs", "direct", { durable: true });
 
   // create queues
-  await channel.assertQueue("processing.requests", { durable: true });
-  await channel.assertQueue("processing.results", { durable: true });
+  //await channel.assertQueue("processing.requests", { durable: true });
+  //await channel.assertQueue("processing.results", { durable: true });
+  await channel.assertQueue("topic_logs.atlantico", { durable: true });
+  await channel.assertQueue("topic_logs.pacifico", { durable: true });
+  await channel.assertQueue("topic_logs.indico", { durable: true });
 
   // bind queues
-  await channel.bindQueue("processing.requests","processing", "request");
-  await channel.bindQueue("processing.results","processing", "result");
+  //await channel.bindQueue("processing.requests","processing", "request");
+  //await channel.bindQueue("processing.results","processing", "result");
+  await channel.bindQueue("topic_logs.atlantico","topic_logs", "atlantico");
+  await channel.bindQueue("topic_logs.pacifico","topic_logs", "pacifico");
+  await channel.bindQueue("topic_logs.indico","topic_logs", "indico");
 
   console.log("Setup DONE");
   process.exit();
--- web-service/web-service.js
+++ web-service/web-service.js
@@ -24,23 +24,30 @@ app.post('/api/v1/processData', async function (req, res) {
   // connect to Rabbit MQ and create a channel
   let connection = await amqp.connect(messageQueueConnectionString);
   let channel = await connection.createConfirmChannel();
+  let key = req.body.key;
 
   // publish the data to Rabbit MQ
-  await publishToChannel(channel, { routingKey: "request", exchangeName: "processing", data: req.body });
+  //await publishToChannel(channel, { routingKey: "request", exchangeName: "processing", data: req.body });
+  await publishToChannel(channel, { routingKey: key, exchangeName: "topic_logs", data: req.body });
 
   // send the request id in the response
   //res.send({ requestId })
+  
   res.sendStatus(200);
+  channel.close();
+  connection.close();
 });
+
 
 app.get('/api/v1/acidification', async function (req, res) {
   console.log("aqui");
   // connect to Rabbit MQ and create a channel
   let connection = await amqp.connect(messageQueueConnectionString);
   let channel = await connection.createConfirmChannel();
+  await channel.prefetch(1);
 
   return new Promise((resolve, reject) => {
-    channel.consume("processing.requests", async function (msg) {
+    channel.consume("topic_logs.pacifico", async function (msg) {
       // parse message
       let msgBody = msg.content.toString();
       let data = JSON.parse(msgBody);
@@ -68,6 +75,7 @@ app.get('/api/v1/acidification', async function (req, res) {
   // let data = await consume({ connection, channel });
   // console.log("RES: ", data);
 });
+
 
 // utility function to publish messages to a channel
 function publishToChannel(channel, { routingKey, exchangeName, data }) {
