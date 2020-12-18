ec9beb6a9f6aab70027117cc29fc0f3cb8db7729
Marcelo Rose <marcelo.rose@developweb.com.br>
2020-12-18 00:56:52 -0300

    Alterações finais

--- web-service/web-service.js
+++ web-service/web-service.js
@@ -8,55 +8,35 @@ const bodyParser = require('body-parser');
 const amqp = require('amqplib');
 const cors = require('cors');
 
-// Middleware
 app.use(bodyParser.json());
 
 app.use(cors())
 
-// RabbitMQ connection string
 const messageQueueConnectionString = process.env.CLOUDAMQP_URL;
 
-// handle the request
-
-
-// handle the request
 app.post('/api/v1/processData', async function (req, res) {
 
- // connect to Rabbit MQ and create a channel
  let connection = await amqp.connect(messageQueueConnectionString);
  let channel = await connection.createConfirmChannel();
  let key = req.body.key;
 
- // publish the data to Rabbit MQ
- //await publishToChannel(channel, { routingKey: "request", exchangeName: "processing", data: req.body });
  await publishToChannel(channel, { routingKey: key, exchangeName: "topic_logs", data: req.body });
-
- // send the request id in the response
- //res.send({ requestId })
  
  res.sendStatus(200);
  channel.close();
  connection.close();
 });
 
-
-
-
-
 app.get('/api/v1/acidification', async function (req, res) {
 
-  console.log("aqui");
-  // connect to Rabbit MQ and create a channel
   let connection = await amqp.connect(messageQueueConnectionString);
   let channel = await connection.createConfirmChannel();
   await channel.prefetch(1);
 
    return  new Promise((resolve, reject) => {
-    channel.consume("topic_logs.pacifico", async function (msg) {
-      // parse message
+    channel.consume("topic_logs." + req.query.oceano, async function (msg) {
       let msgBody = msg.content.toString();
       let data = JSON.parse(msgBody);
-      console.log("CONSUME: ", msgBody);      
       await channel.ack(msg);
       channel.close();
      
@@ -66,12 +46,10 @@ app.get('/api/v1/acidification', async function (req, res) {
 
     });
 
-    // handle connection closed
     connection.on("close", (err) => {
       return reject(err);
     });
 
-    // handle errors
     connection.on("error", (err) => {
       return reject(err);
     });
@@ -79,8 +57,6 @@ app.get('/api/v1/acidification', async function (req, res) {
   
 });
 
-
-// utility function to publish messages to a channel
 function publishToChannel(channel, { routingKey, exchangeName, data }) {
   return new Promise((resolve, reject) => {
     channel.publish(exchangeName, routingKey, Buffer.from(JSON.stringify(data), 'utf-8'), { persistent: true }, function (err, ok) {
@@ -93,9 +69,6 @@ function publishToChannel(channel, { routingKey, exchangeName, data }) {
   });
 }
 
-
-
-// Start the server
 const PORT = 3000;
 server = http.createServer(app);
 server.listen(PORT, "localhost", function (err) {
@@ -105,6 +78,3 @@ server.listen(PORT, "localhost", function (err) {
     console.info("Listening on port %s.", PORT);
   }
 });
-
-// listen for results on RabbitMQ
-//listenForResults();
